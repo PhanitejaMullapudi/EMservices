643b128f08ccb428bdf895fa5564dc268bbaa8bf
Phaniteja.M <phaniteja1989@gmail.com>
2017-06-01 15:22:41 +0530

    Architectureal changes

---
+++ DataLayer/UOW/UnitOfWork.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Context;
+namespace DataLayer.UOW
+{
+    public class UnitOfWork : IDisposable
+    {
+        #region"variable            "
+        private ExpensesManagerDB context;
+        private bool disposed = false;
+        #endregion
+
+
+
+        #region"constructor         "
+        public UnitOfWork(ExpensesManagerDB DB)
+        {
+            this.context = DB;
+        }
+        #endregion
+
+        #region"Save                "
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+        #endregion
+
+        #region"Dispose             "
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+        #endregion
+
+        #region"Dispose             "
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
